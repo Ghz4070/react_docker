bf047060f4c2feae532653e446cf2ab0f96ab799
Ghz4070 <ghezal-ilies@outlook.fr>
2021-01-27 02:43:19 +0100

    fixed redux -> push in array object data in forms and get role ADMIN since auth

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,10 +1,15 @@
 export const ACTION_SET_USER = "ACTION_SET_USER";
 export const ACTION_SET_USER_TEMPORARY = "ACTION_SET_USER_TEMPORARY";
 export const ACTION_RESET_USER_TEMPORARY = "ACTION_RESET_USER_TEMPORARY";
+export const ACTION_RESET_USER = "ACTION_RESET_USER";
 
 export const actionSetUser = (payload) => ({
   type: ACTION_SET_USER,
   payload,
+});
+
+export const actionResetUser = () => ({
+  type: ACTION_RESET_USER,
 });
 
 export const actionSetUserTemporary = (payload) => ({
--- store/reducers/auth.js
+++ store/reducers/auth.js
@@ -3,10 +3,11 @@ import {
   ACTION_SET_USER,
   ACTION_SET_USER_TEMPORARY,
   ACTION_RESET_USER_TEMPORARY,
+  ACTION_RESET_USER,
 } from "../actions/auth";
 
 const initialState = {
-  profile: { name: "", role: "" },
+  profile: {},
   nameTemporary: "",
 };
 
@@ -15,6 +16,9 @@ const auth = (state = initialState, action) => {
     switch (action.type) {
       case ACTION_SET_USER:
         draft.profile = action.payload;
+        break;
+      case ACTION_RESET_USER:
+        draft.profile = {};
         break;
       case ACTION_SET_USER_TEMPORARY:
         draft.nameTemporary = action.payload;
